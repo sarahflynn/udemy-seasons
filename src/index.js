e4e7f988729bae8834a4ef78c79140d10ea274b5
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-24 10:38:51 -0800

    season component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
-  return <div>Hi There</div>;
+  return <div>Hi There
+    <SeasonDisplay />
+  </div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
