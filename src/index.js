fdded9ae571a55663032c766dec0a8c89fd40235
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-24 19:36:35 -0800

    state and lifecycle methods

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  return <div>Hi There
-    <SeasonDisplay />
-  </div>;
-};
+class App extends React.Component {
+  state = { lat: null, errorMessage: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Latitude: {this.state.lat}</h3>
+        <h3>Error: {this.state.errorMessage}</h3>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
