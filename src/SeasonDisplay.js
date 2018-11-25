3172e2ea12fefd14d182f525f945d877e5300c6c
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-25 05:34:43 -0800

    season config in season display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    message: "Let's hit the beach",
+    img: 'http://icons.iconarchive.com/icons/dapino/beach/256/sun-icon.png',
+    alt: 'sun'
+  },
+  winter: {
+    message: "Burr, it's chilly",
+    img:
+      'http://icons.iconarchive.com/icons/psdblast/flat-christmas/256/snowflake-icon.png',
+    alt: 'snowflake'
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -10,9 +24,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const { message, img, alt} = seasonConfig[season];
 
   return (
-    <div>Season Display: {season}</div>
+    <div>
+      <img src={img} alt={alt} />
+      <h1>{message}</h1>
+      <img src={img} alt={alt} />
+    </div>
   );
 };
 
